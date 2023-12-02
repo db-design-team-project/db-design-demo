fb25ef953cff4c8fa9d2c975d7a444da4d26ad8f
Jehee Cheon <jeheecheon@gmail.com>
2023-12-03 02:05:06 +0900

    로그인시 db 조회

--- src/Server/Controllers/AuthController.cs
+++ src/Server/Controllers/AuthController.cs
@@ -5,16 +5,20 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Server.DbContexts;
 
 [ApiController]
 [Route("api/[controller]")]
 public class AuthController : ControllerBase {
     private readonly ILogger _logger;
+    private readonly MainDbContext _dbContext;
 
     public AuthController(
-        ILogger<TestController> logger
+        ILogger<TestController> logger,
+        MainDbContext dbContext
     ) {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     [HttpPost("login")]
@@ -23,12 +27,18 @@ public class AuthController : ControllerBase {
         string password = json.GetString("password") ?? string.Empty;
 
         // 유저이름이나 비밀번호가 비어있거나 null 인 경우
-        if (string.IsNullOrWhiteSpace(ID) || string.IsNullOrWhiteSpace(password)) {
-            return BadRequest("Invalid username or password");
-        }
+        if (string.IsNullOrWhiteSpace(ID) || string.IsNullOrWhiteSpace(password))
+            return BadRequest(JsonSerializer.Serialize(new {
+                message = "아이디, 패스워드가 비어있습니다..."
+            }));
 
         // TODO: DB 조회해서 유요한 id/password 인지 확인
-        
+        var results = _dbContext.회원DbSet.FromSqlInterpolated($"SELECT * FROM 회원 WHERE 아이디 = {ID} AND 비밀번호 = {123}");
+        if (results is null)
+            return BadRequest(JsonSerializer.Serialize(new {
+                message = "유효한 아이디, 패스워드가 아닙니다..."
+            }));
+
         await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(
             new ClaimsIdentity(
                 new Claim[] {
@@ -38,7 +48,9 @@ public class AuthController : ControllerBase {
             )
         ));
 
-        return Ok();
+        return Ok(JsonSerializer.Serialize(new {
+            message = "로그인 성공!"
+        }));
     }
 
     // [HttpPost("signup")]
